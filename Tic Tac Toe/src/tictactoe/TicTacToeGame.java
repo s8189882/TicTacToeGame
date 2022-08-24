3dd408b02e1820a926da4dde3ce44bfe0df6d3f0
s8189882 <s8189882@gmail.com>
2022-08-24 21:01:04 +0530

    Add main - Display welcome message

---
+++ Tic Tac Toe/src/tictactoe/TicTacToeGame.java
@@ -0,0 +1,10 @@
+package tictactoe;
+
+public class TicTacToeGame {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Tic Tac Toe Game Program in Java!");
+		System.out.println("In this program you will compete to win in a Tic Tac Toe game against the Computer.\n");
+		
+	}
+}
