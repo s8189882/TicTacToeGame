7771516f6cc51630a1e92063b07ddcd38cc872aa
s8189882 <s8189882@gmail.com>
2022-08-25 17:54:48 +0530

    Add UC2 - Player chooses to play X or O

--- Tic Tac Toe/src/tictactoe/TicTacToeGame.java
+++ Tic Tac Toe/src/tictactoe/TicTacToeGame.java
@@ -1,7 +1,11 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	char gameBoard[] = null;
+	char playerLetter = '\0';
+	char computerLetter = '\0';
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game Program in Java!");
@@ -9,6 +13,7 @@ public class TicTacToeGame {
 		
 		TicTacToeGame gameObj = new TicTacToeGame();
 		gameObj.initialiseGame();
+		gameObj.choosePlayerLetter();
 	}
 	
 	void initialiseGame(){
@@ -16,4 +21,23 @@ public class TicTacToeGame {
 		for(int index = 0; index < 10; index++)
 			gameBoard[index] = ' ';
 	}	
+	
+	void choosePlayerLetter() {
+		System.out.println("\nPlayer please choose your play Letter.");
+		System.out.println("Enter 'X' to play 'X' on your turn.");
+		System.out.println("Or Enter 'O' to play 'O' on your turn.");
+		Scanner sc = new Scanner(System.in);
+		char playerInput = sc.next().charAt(0);
+		sc.close();
+		if (playerInput == 'X' || playerInput == 'x') {
+			playerLetter = 'X';
+			computerLetter = 'O';
+		}
+		else if (playerInput == 'O' || playerInput == 'o') {
+			playerLetter = 'O';
+			computerLetter = 'o';
+		}
+		else
+			System.out.println("\nInvalid Input.\nPlease try again!");
+	}
 }
