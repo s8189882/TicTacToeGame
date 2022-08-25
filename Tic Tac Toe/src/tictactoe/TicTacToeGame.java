b6820d162b851b79c3442e9a0ae03bc1206a68a8
s8189882 <s8189882@gmail.com>
2022-08-25 20:14:50 +0530

    Add UC6 - toss to check who plays first  the computer or the player

--- Tic Tac Toe/src/tictactoe/TicTacToeGame.java
+++ Tic Tac Toe/src/tictactoe/TicTacToeGame.java
@@ -15,6 +15,10 @@ public class TicTacToeGame {
 		
 		TicTacToeGame gameObj = new TicTacToeGame();
 		gameObj.initialiseGame();
+		if (gameObj.toss() == 1)
+			System.out.println("\nPlayer won the toss.\nPlayer plays first.");
+		else
+			System.out.println("\nComputer won the toss.\nComputer plays first.");
 		gameObj.choosePlayerLetter();
 		gameObj.showBoard();
 		gameObj.playerPlays();
@@ -68,5 +72,9 @@ public class TicTacToeGame {
 		}
 		else
 			gameBoard[playerCell] = playerLetter;
+	}
+	
+	int toss() {
+		return (Math.random() > 0.5 ? 1 : 0);
 	}
 }
