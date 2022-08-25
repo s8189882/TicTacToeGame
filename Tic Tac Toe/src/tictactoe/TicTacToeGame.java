8d72537cccda5316a19b57ee31c47249c638daae
s8189882 <s8189882@gmail.com>
2022-08-25 17:35:00 +0530

    Add UC1 - create game board

--- Tic Tac Toe/src/tictactoe/TicTacToeGame.java
+++ Tic Tac Toe/src/tictactoe/TicTacToeGame.java
@@ -1,10 +1,19 @@
 package tictactoe;
 
 public class TicTacToeGame {
+	char gameBoard[] = null;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game Program in Java!");
 		System.out.println("In this program you will compete to win in a Tic Tac Toe game against the Computer.\n");
 		
+		TicTacToeGame gameObj = new TicTacToeGame();
+		gameObj.initialiseGame();
 	}
+	
+	void initialiseGame(){
+		gameBoard = new char[10];
+		for(int index = 0; index < 10; index++)
+			gameBoard[index] = ' ';
+	}	
 }
