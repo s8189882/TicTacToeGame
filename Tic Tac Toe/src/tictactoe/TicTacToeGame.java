3225dd635f1b338b4f6c400a2467dc284c866575
s8189882 <s8189882@gmail.com>
2022-08-25 18:21:36 +0530

    Add UC3 - display current Board so player can choose the valid cells to make move

--- Tic Tac Toe/src/tictactoe/TicTacToeGame.java
+++ Tic Tac Toe/src/tictactoe/TicTacToeGame.java
@@ -14,6 +14,7 @@ public class TicTacToeGame {
 		TicTacToeGame gameObj = new TicTacToeGame();
 		gameObj.initialiseGame();
 		gameObj.choosePlayerLetter();
+		gameObj.showBoard();
 	}
 	
 	void initialiseGame(){
@@ -39,5 +40,16 @@ public class TicTacToeGame {
 		}
 		else
 			System.out.println("\nInvalid Input.\nPlease try again!");
+	}
+	
+	void showBoard() {
+		System.out.println("\nCurrent Board : ");
+		System.out.println("-------------");
+		System.out.println("| "+gameBoard[1]+" | "+gameBoard[2]+" | "+gameBoard[3]+" |");
+		System.out.println("-------------");
+		System.out.println("| "+gameBoard[4]+" | "+gameBoard[5]+" | "+gameBoard[6]+" |");
+		System.out.println("-------------");
+		System.out.println("| "+gameBoard[7]+" | "+gameBoard[8]+" | "+gameBoard[9]+" |");
+		System.out.println("-------------");
 	}
 }
