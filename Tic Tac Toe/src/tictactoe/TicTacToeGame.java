1fc9b90452e02eed1fa386bd3ceb06e363490746
s8189882 <s8189882@gmail.com>
2022-08-26 18:27:58 +0530

    Add UC7 - check after every move the winner or tie or change the turn

--- Tic Tac Toe/src/tictactoe/TicTacToeGame.java
+++ Tic Tac Toe/src/tictactoe/TicTacToeGame.java
@@ -6,6 +6,7 @@ public class TicTacToeGame {
 	char gameBoard[] = null;
 	char playerLetter = '\0';
 	char computerLetter = '\0';
+	String winner = null;
 	static Scanner sc = new Scanner(System.in);
 
 
@@ -23,6 +24,7 @@ public class TicTacToeGame {
 		gameObj.showBoard();
 		gameObj.playerPlays();
 		gameObj.showBoard();
+		gameObj.checkWinOrTie();
 	}
 	
 	void initialiseGame(){
@@ -44,8 +46,10 @@ public class TicTacToeGame {
 			playerLetter = 'O';
 			computerLetter = 'o';
 		}
-		else
+		else {
 			System.out.println("\nInvalid Input.\nPlease try again!");
+			choosePlayerLetter();
+		}
 	}
 	
 	void showBoard() {
@@ -77,4 +81,69 @@ public class TicTacToeGame {
 	int toss() {
 		return (Math.random() > 0.5 ? 1 : 0);
 	}
+	
+	void checkWinOrTie() {
+		char winLetter = ' ';
+		byte cellsStillEmpty = 0;
+		winner = null;
+		
+		for(int pattern = 1; pattern < 9; pattern++) {
+			switch(pattern) {
+				case 1 :
+					if(gameBoard[1] == gameBoard[2] && gameBoard[1] == gameBoard[3])
+						winLetter = gameBoard[1];
+					break;
+				case 2 :
+					if(gameBoard[4] == gameBoard[5] && gameBoard[4] == gameBoard[6])
+						winLetter = gameBoard[4];
+					break;
+				case 3 :
+					if(gameBoard[7] == gameBoard[8] && gameBoard[7] == gameBoard[9])
+						winLetter = gameBoard[7];
+					break;	
+				case 4 :
+					if(gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7])
+						winLetter = gameBoard[1];
+					break;
+				case 5 :
+					if(gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8])
+						winLetter = gameBoard[2];
+					break;
+				case 6 :
+					if(gameBoard[3] == gameBoard[6] && gameBoard[3] == gameBoard[9])
+						winLetter = gameBoard[3];
+					break;
+				case 7 :
+					if(gameBoard[1] == gameBoard[5] && gameBoard[1] == gameBoard[9])
+						winLetter = gameBoard[1];
+					break;
+				case 8 :
+					if(gameBoard[3] == gameBoard[5] && gameBoard[3] == gameBoard[7])
+						winLetter = gameBoard[3];
+					break;
+			}
+		}
+			winner = "me";
+		if (winLetter == playerLetter)
+			winner = "PLAYER";
+		else if(winLetter == computerLetter)
+			winner = "COMPUTER";
+		else {
+			for(int counter = 1; counter <= 9; counter++) {
+				if(gameBoard[counter] == ' ')
+					cellsStillEmpty++;
+			}
+			if (cellsStillEmpty == 0)
+				winner = "TIE";
+		}
+		if (winner != null)
+			displayResults();
+	}
+
+	void displayResults() {
+		if (winner == "TIE") 
+			System.out.println("\n\nIt's a TIE!\nPlease try again.");
+		else 
+			System.out.println("\n\n"+winner+" wins!");
+	}	
 }
