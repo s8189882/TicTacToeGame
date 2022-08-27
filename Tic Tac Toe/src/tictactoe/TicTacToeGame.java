0225ffe0e8267051b647812bce639b69adae12c4
s8189882 <s8189882@gmail.com>
2022-08-27 21:30:51 +0530

    Add UC10 - If neither is winning then take one of the available corners

--- Tic Tac Toe/src/tictactoe/TicTacToeGame.java
+++ Tic Tac Toe/src/tictactoe/TicTacToeGame.java
@@ -277,59 +277,57 @@ public class TicTacToeGame {
 	}
 	
 	void blockPlayersMove() {
-		if(gameBoard[1] == playerLetter && gameBoard[2] == playerLetter && gameBoard[3] == ' ')
-			gameBoard[3] = computerLetter;
-		else if(gameBoard[2] == playerLetter && gameBoard[3] == playerLetter && gameBoard[1] == ' ')
+		if(gameBoard[2] == playerLetter && gameBoard[3] == playerLetter && gameBoard[1] == ' ')
 			gameBoard[1] = computerLetter;
-		else if(gameBoard[1] == playerLetter && gameBoard[3] == playerLetter && gameBoard[2] == ' ')
-			gameBoard[2] = computerLetter;
+		else if(gameBoard[4] == playerLetter && gameBoard[7] == playerLetter && gameBoard[1] == ' ')
+			gameBoard[1] = computerLetter;
+		else if(gameBoard[5] == playerLetter && gameBoard[9] == playerLetter && gameBoard[1] == ' ')
+			gameBoard[1] = computerLetter;
 		
+		else if(gameBoard[1] == playerLetter && gameBoard[2] == playerLetter && gameBoard[3] == ' ')
+			gameBoard[3] = computerLetter; 
+		else if(gameBoard[6] == playerLetter && gameBoard[9] == playerLetter && gameBoard[3] == ' ')
+			gameBoard[3] = computerLetter;
+		else if(gameBoard[5] == playerLetter && gameBoard[7] == playerLetter && gameBoard[3] == ' ')
+			gameBoard[3] = computerLetter;
+		
+		else if(gameBoard[8] == playerLetter && gameBoard[9] == playerLetter && gameBoard[7] == ' ')
+			gameBoard[7] = computerLetter;
+		else if(gameBoard[1] == playerLetter && gameBoard[4] == playerLetter && gameBoard[7] == ' ')
+			gameBoard[7] = computerLetter;
+		else if(gameBoard[3] == playerLetter && gameBoard[5] == playerLetter && gameBoard[7] == ' ')
+			gameBoard[7] = computerLetter;
+		
+		else if(gameBoard[7] == playerLetter && gameBoard[8] == playerLetter && gameBoard[9] == ' ')
+			gameBoard[9] = computerLetter;
+		else if(gameBoard[3] == playerLetter && gameBoard[6] == playerLetter && gameBoard[9] == ' ')
+			gameBoard[9] = computerLetter;
+		else if(gameBoard[1] == playerLetter && gameBoard[5] == playerLetter && gameBoard[9] == ' ')
+			gameBoard[9] = computerLetter;
+		
+		
+		else if(gameBoard[1] == playerLetter && gameBoard[3] == playerLetter && gameBoard[2] == ' ')
+			gameBoard[2] = computerLetter;	
 		else if(gameBoard[4] == playerLetter && gameBoard[5] == playerLetter && gameBoard[6] == ' ')
 			gameBoard[6] = computerLetter;
 		else if(gameBoard[5] == playerLetter && gameBoard[6] == playerLetter && gameBoard[4] == ' ')
 			gameBoard[4] = computerLetter;
 		else if(gameBoard[4] == playerLetter && gameBoard[6] == playerLetter && gameBoard[5] == ' ')
 			gameBoard[5] = computerLetter;
-		
-		else if(gameBoard[7] == playerLetter && gameBoard[8] == playerLetter && gameBoard[9] == ' ')
-			gameBoard[9] = computerLetter;
-		else if(gameBoard[8] == playerLetter && gameBoard[9] == playerLetter && gameBoard[7] == ' ')
-			gameBoard[7] = computerLetter;
 		else if(gameBoard[7] == playerLetter && gameBoard[9] == playerLetter && gameBoard[8] == ' ')
 			gameBoard[8] = computerLetter;
-		
-		else if(gameBoard[1] == playerLetter && gameBoard[4] == playerLetter && gameBoard[7] == ' ')
-			gameBoard[7] = computerLetter;
-		else if(gameBoard[4] == playerLetter && gameBoard[7] == playerLetter && gameBoard[1] == ' ')
-			gameBoard[1] = computerLetter;
 		else if(gameBoard[1] == playerLetter && gameBoard[7] == playerLetter && gameBoard[4] == ' ')
-			gameBoard[4] = computerLetter;
-		
+			gameBoard[4] = computerLetter;	
 		else if(gameBoard[2] == playerLetter && gameBoard[5] == playerLetter && gameBoard[8] == ' ')
 			gameBoard[8] = computerLetter;
 		else if(gameBoard[5] == playerLetter && gameBoard[8] == playerLetter && gameBoard[2] == ' ')
 			gameBoard[2] = computerLetter;
 		else if(gameBoard[2] == playerLetter && gameBoard[8] == playerLetter && gameBoard[5] == ' ')
 			gameBoard[5] = computerLetter;
-		
-		else if(gameBoard[3] == playerLetter && gameBoard[6] == playerLetter && gameBoard[9] == ' ')
-			gameBoard[9] = computerLetter;
-		else if(gameBoard[6] == playerLetter && gameBoard[9] == playerLetter && gameBoard[3] == ' ')
-			gameBoard[3] = computerLetter;
 		else if(gameBoard[3] == playerLetter && gameBoard[9] == playerLetter && gameBoard[6] == ' ')
 			gameBoard[6] = computerLetter;
-		
-		else if(gameBoard[1] == playerLetter && gameBoard[5] == playerLetter && gameBoard[9] == ' ')
-			gameBoard[9] = computerLetter;
-		else if(gameBoard[5] == playerLetter && gameBoard[9] == playerLetter && gameBoard[1] == ' ')
-			gameBoard[1] = computerLetter;
 		else if(gameBoard[1] == playerLetter && gameBoard[9] == playerLetter && gameBoard[5] == ' ')
 			gameBoard[5] = computerLetter;
-		
-		else if(gameBoard[3] == playerLetter && gameBoard[5] == playerLetter && gameBoard[7] == ' ')
-			gameBoard[7] = computerLetter;
-		else if(gameBoard[5] == playerLetter && gameBoard[7] == playerLetter && gameBoard[3] == ' ')
-			gameBoard[3] = computerLetter;
 		else if(gameBoard[3] == playerLetter && gameBoard[7] == playerLetter && gameBoard[5] == ' ')
 			gameBoard[5] = computerLetter;
 	}
